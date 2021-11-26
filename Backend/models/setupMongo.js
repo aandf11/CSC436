d4c40245d1dea1a10225a5e9a8326c6d23167ca8
aandf11 <aidenfung1994@gmail.com>
2021-11-26 17:29:58 -0600

    Update setupMongo.js

--- Backend/models/setupMongo.js
+++ Backend/models/setupMongo.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-//const uri = "mongodb+srv://admin:9IQ8sSiEMdLIqDUs@cluster0.zoenx.mongodb.net/Todolist?retryWrites=true&w=majority";
 const uri = process.env.DB_URI;
 
 function connect() {
